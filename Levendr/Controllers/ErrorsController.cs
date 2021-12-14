1af080f8a15e0fd21087ab868c1b9b91d903e910
jzorjnz <akhtar.syedzeeshan@gmail.com>
2021-12-14 18:59:00 +0500

    Added Errors Controller

---
+++ Levendr/Controllers/ErrorsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+using Levendr.Services;
+using Levendr.Models;
+using Levendr.Enums;
+using Levendr.Constants;
+using Levendr.Helpers;
+using Levendr.Interfaces;
+using Levendr.Exceptions;
+using Levendr.Filters;
+using System.Security.Claims;
+
+namespace Levendr.Controllers
+{
+    [ApiController]
+    [Route("API/[controller]")]
+    public class ErrorsController : ControllerBase
+    {
+        private readonly ILogger<ErrorsController> _logger;
+
+        public ErrorsController(ILogger<ErrorsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Error")]
+        public APIResult Error(string errorCode, string errorMessage)
+        {
+            return new APIResult {
+                Success = false,
+                Data = null,
+                ErrorCode = "AUTH001",
+                Message = errorMessage
+            };
+        }
+    }
+}
